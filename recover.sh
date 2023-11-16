b45b03a4219db5a52b2f14e2ca56bb1ac17a5c8b
James Beegen <jamesbeegen@gmail.com>
2023-11-16 11:09:46 -0500

    initial commit

---
+++ recover.sh
@@ -0,0 +1,22 @@
+pacman -S wget unzip zsh
+
+# oh my posh
+curl -s https://ohmyposh.dev/install.sh | sudo bash -s
+
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# chsh
+chsh -s /usr/bin/zsh
+
+# install plugins
+cd $HOME/.oh-my-zsh/custom/plugins/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+git clone https://github.com/clarketm/zsh-completions.git
+
+# Get theme
+cp theme.json $HOME/.ohmyposh/themes/theme.json
+
+# Copy zshrc
+cp .zshrc $HOME/.zshrc
