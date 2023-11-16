2c93884e9384f3bd4f62feb392a2781339c4776b
jamesbeegen <77640214+jamesbeegen@users.noreply.github.com>
2023-11-16 11:35:11 -0500

    Update recover.sh

--- recover.sh
+++ recover.sh
@@ -6,19 +6,15 @@ curl -s https://ohmyposh.dev/install.sh | sudo bash -s
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# chsh
-chsh -s /usr/bin/zsh
-
-# install plugins
-cd $HOME/.oh-my-zsh/custom/plugins/ && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-
-cd $HOME/.oh-my-zsh/custom/plugins/&& git clone https://github.com/zsh-users/zsh-autosuggestions.git
-cd $HOME/.oh-my-zsh/custom/plugins/&& git clone https://github.com/clarketm/zsh-completions.git
-
 # Get theme
 mkdir $HOME/.ohmyposh/
 mkdir $HOME/.ohmyposh/themes/
 cp theme.json $HOME/.ohmyposh/themes/theme.json
+
+# install plugins
+cd $HOME/.oh-my-zsh/custom/plugins/ && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+cd $HOME/.oh-my-zsh/custom/plugins/ && git clone https://github.com/zsh-users/zsh-autosuggestions.git
+cd $HOME/.oh-my-zsh/custom/plugins/ && git clone https://github.com/clarketm/zsh-completions.git
 
 # Copy zshrc
 cp .zshrc $HOME/.zshrc
